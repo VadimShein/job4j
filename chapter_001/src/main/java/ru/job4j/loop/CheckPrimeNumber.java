eecf1d9e100e7374afe52c4ad84dd5aa1f5fbf4d
VadimShein <shein.v94@mail.ru>
2020-03-03 01:56:11 +0600

    Простое число. [#241152]

--- chapter_001/src/main/java/ru/job4j/loop/CheckPrimeNumber.java
+++ chapter_001/src/main/java/ru/job4j/loop/CheckPrimeNumber.java
@@ -8,7 +8,7 @@ public class CheckPrimeNumber {
             prime = false;
         }
         for (int i = 2; i < number; i++) {
-            if(((number % i ) == 0)  ) {
+            if((number % i ) == 0)   {
                 prime = false;
                 break;
             }
