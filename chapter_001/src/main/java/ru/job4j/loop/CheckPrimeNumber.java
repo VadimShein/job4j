8ad5412c3b5400744378d2208cac50bd8e524f19
VadimShein <shein.v94@mail.ru>
2020-03-03 01:46:41 +0600

    Простое число [#241152]

--- chapter_001/src/main/java/ru/job4j/loop/CheckPrimeNumber.java
+++ chapter_001/src/main/java/ru/job4j/loop/CheckPrimeNumber.java
@@ -4,6 +4,9 @@ public class CheckPrimeNumber {
 
     public static boolean check(int number) {
         boolean prime = true;
+        if (number ==1) {
+            prime = false;
+        }
         for (int i = 2; i < number; i++) {
             if(((number % i ) == 0)  ) {
                 prime = false;
--- chapter_001/src/test/java/ru/job4j/loop/CheckPrimeNumberTest.java
+++ chapter_001/src/test/java/ru/job4j/loop/CheckPrimeNumberTest.java
@@ -21,7 +21,7 @@ public class CheckPrimeNumberTest {
     @Test
     public void when1() {
         boolean rsl = CheckPrimeNumber.check(1);
-        assertThat(rsl, is(true));
+        assertThat(rsl, is(false));
     }
 
 }
