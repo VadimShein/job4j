5166b762358408899d3fd207847c83db6cf2ddc6
VadimShein <shein.v94@mail.ru>
2020-03-06 13:46:53 +0600

    Слово заканчивается с ...[#241085]

--- chapter_001/src/main/java/ru/job4j/array/ArrayChar.java
+++ chapter_001/src/main/java/ru/job4j/array/ArrayChar.java
@@ -1,10 +1,10 @@
 package ru.job4j.array;
 
 public class ArrayChar {
-    public static boolean startsWith(char[] word, char[] pref) {
+    public static boolean startsWith(char[] world, char[] pref) {
         boolean result = true;
         for (int i = 0; i < pref.length; i++) {
-            if (word[i] != pref[i]) {
+            if (world[i] != pref[i]) {
                 result = false;
                 break;
             }
--- chapter_001/src/main/java/ru/job4j/array/EndsWith.java
+++ chapter_001/src/main/java/ru/job4j/array/EndsWith.java
@@ -3,11 +3,12 @@ package ru.job4j.array;
 public class EndsWith {
     public static boolean endsWith(char[] word, char[] post) {
         boolean result = true;
-        for (int i = 0; i < pref.length; i++) {
-            if (world[i] != pref[i]) {
+        for (int i = 0; i < post.length; i++) {
+            if (word[word.length - i - 1] != post[post.length - i - 1]) {
                 result = false;
                 break;
             }
+        }
         return result;
     }
 }
