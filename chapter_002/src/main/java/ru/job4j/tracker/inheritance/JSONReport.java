fc1213c24467bdc8085af6e2a034f1eaaeb47a41
VadimShein <shein.v94@mail.ru>
2020-03-22 20:19:29 +0600

    4. Переопределение[#241261]

--- chapter_002/src/main/java/ru/job4j/tracker/inheritance/JSONReport.java
+++ chapter_002/src/main/java/ru/job4j/tracker/inheritance/JSONReport.java
@@ -1,8 +1,11 @@
 package ru.job4j.tracker.inheritance;
 
 public class JSONReport extends TextReport {
+    @SuppressWarnings("checkstyle:OperatorWrap")
     @Override
     public String generate(String name, String body) {
-        return "name: " + name + System.lineSeparator() + "body: " + body;
+        return "{" + System.lineSeparator()
+                + "name: " + name + System.lineSeparator() + "body: " + body
+                + System.lineSeparator() + "}";
     }
 }
