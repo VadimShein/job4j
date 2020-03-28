ca962ef749f359c8e888a51bd1de293564d07f1f
VadimShein <shein.v94@mail.ru>
2020-03-28 15:54:36 +0600

    5. Реализовать класс Tracker[#241272]

--- chapter_002/src/main/java/ru/job4j/tracker/Tracker.java
+++ chapter_002/src/main/java/ru/job4j/tracker/Tracker.java
@@ -15,67 +15,39 @@ public class Tracker {
         return item;
     }
 
-    //получение списка всех заявок
     public Item[] findAll() {
         Item[] namesWithoutNull = new Item[position];
         int size = 0;
         for (int index =  0; index < position; index++) {
             if (items[index].getName() != null) {
-                namesWithoutNull[size].setName(items[index].getName());
-                namesWithoutNull[size].setId(items[index].getId());
-                size++;
-            } else {
-                namesWithoutNull[index].setName("null");
-                namesWithoutNull[index].setId("null");
+                namesWithoutNull[size++] = items[index];
             }
         }
-        namesWithoutNull = Arrays.copyOf(namesWithoutNull, size);
-        for (int index = 0; index < namesWithoutNull.length; index++) {
-            System.out.println("findAll " + namesWithoutNull[index].getName());
-        }
-        return namesWithoutNull;
+        return Arrays.copyOf(namesWithoutNull, size);
     }
 
-
-     //получение списка по имени
     public Item[] findByName(String key) {
         Item[] foundNames = new Item[position];
         int size = 0;
         for (int index =  0; index < position; index++) {
-            if (items[index].getName().equals(key)) {
-                foundNames[size].setName(items[index].getName());
-                foundNames[size].setId(items[index].getId());
-                size++;
-            } else {
-                foundNames[index].setName("null");
-                foundNames[index].setId("null");
-            }
+           if (items[index].getName().equals(key)) {
+               foundNames[size++] = items[index];
+           }
         }
-        foundNames = Arrays.copyOf(foundNames, size);
-        for (int index = 0; index < foundNames.length; index++) {
-            System.out.println("findByName " + foundNames[index].getName());
-        }
-        return foundNames;
+        return Arrays.copyOf(foundNames, size);
     }
 
-    //получение заявки по id
     public Item findById(String id) {
         Item foundId = new Item(null);
         for (int index =  0; index < position; index++) {
             if (items[index].getId().equals(id)) {
-                foundId.setName(items[index].getName());
-                foundId.setId(items[index].getId());
-                System.out.println("findById " + foundId.getName() + " " + foundId.getId());
+                foundId = items[index];
                 break;
-            } else {
-                foundId.setName("null");
-                foundId.setId("null");
             }
         }
         return foundId;
     }
 
-    //Метод генерирует уникальный ключ для заявки.
     private String generateId() {
         Random rm = new Random();
         return String.valueOf(rm.nextLong() + System.currentTimeMillis());
--- chapter_002/src/test/java/ru/job4j/tracker/TrackerTest.java
+++ chapter_002/src/test/java/ru/job4j/tracker/TrackerTest.java
@@ -1,9 +1,7 @@
 package ru.job4j.tracker;
 
+import org.junit.Assert;
 import org.junit.Test;
-
-import java.util.Arrays;
-import java.util.Arrays;
 
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
@@ -15,26 +13,52 @@ public class TrackerTest {
         Item item = new Item("test1");
         tracker.add(item);
         Item result = tracker.findById(item.getId());
-    //    assertThat(result.getName(), is(item.getName()));
-        assertThat(result.getId(), is(item.getId()));
+        assertThat(result.getName(), is(item.getName()));
     }
     @Test
     public void idIsNotFound() {
         Tracker tracker = new Tracker();
         Item item = new Item("test1");
         tracker.add(item);
-        Item result = tracker.findById(item.getId() + 1);
-        //assertThat(result.getName(), is(item.getName()));
-        assertThat(result.getId(), is("null"));
+        Item result = tracker.findById(item.getId() + 100);
+        Assert.assertNull(result.getName());
     }
     @Test
     public void nameIsFound() {
         Tracker tracker = new Tracker();
         Item item = new Item("test1");
         tracker.add(item);
-  //      Item item2 = new Item("test1");
-  //      tracker.add(item2);
-        Item[] result =  tracker.findByName(item.getName());
-        assertThat(result[0].getId(), is(item.getId()));
+        Item item2 = new Item("test1");
+        tracker.add(item2);
+        Item[] result = tracker.findByName(item.getName());
+        assertThat(result[0].getName(), is(item.getName()));
+        assertThat(result[1].getName(), is(item2.getName()));
+    }
+    @Test
+    public void nameIsNotFound() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("test1");
+        tracker.add(item);
+        Item item2 = new Item("test1");
+        tracker.add(item2);
+        Item[] result = tracker.findByName("test3");
+        assertThat(result.length, is(0));
+    }
+    @Test
+    public void findAll() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("test1");
+        tracker.add(item);
+        Item item2 = new Item("test2");
+        tracker.add(item2);
+        Item[] result = tracker.findAll();
+        assertThat(result[0].getName(), is(item.getName()));
+        assertThat(result[1].getName(), is(item2.getName()));
+    }
+    @Test
+    public void notFindAll() {
+        Tracker tracker = new Tracker();
+        Item[] result = tracker.findAll();
+        assertThat(result.length, is(0));
     }
 }
