7a86478a12e92a6952a3ce27cbe24a85f4f75608
VadimShein <shein.v94@mail.ru>
2020-03-31 14:41:22 +0600

    3. Интерфейс Input[#241218]

--- chapter_002/src/main/java/ru/job4j/tracker/oop/Student.java
+++ chapter_002/src/main/java/ru/job4j/tracker/oop/Student.java
@@ -1,20 +1,4 @@
 package ru.job4j.tracker.oop;
 
-@SuppressWarnings("checkstyle:WhitespaceAround")
-public class Student {
-
-    public void music() {
-        System.out.println("Tra tra tra");
-    }
-    public void song() {
-        System.out.println("I belive I can fly");
-    }
-    public static void main(String[] args) {
-        Student petya = new Student();
-        petya.music();
-        petya.music();
-        petya.music();
-        petya.song();
-        petya.song();
-        petya.song();    }
+public class Student extends Object {
 }
