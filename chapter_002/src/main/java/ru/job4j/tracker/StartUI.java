292b79e13af763a86971dbed8d9854696f53e45c
VadimShein <shein.v94@mail.ru>
2020-04-01 00:21:31 +0600

    2.1. Реализация класса StartUI[#241227]

--- chapter_002/src/main/java/ru/job4j/tracker/StartUI.java
+++ chapter_002/src/main/java/ru/job4j/tracker/StartUI.java
@@ -7,31 +7,31 @@ public class StartUI {
     public void init(Input input, Tracker tracker) {
         boolean run = true;
         while (run) {
+
+
             System.out.println();
             this.showMenu();
             System.out.print("Select: ");
             int select = Integer.valueOf(input.askStr("Select: "));
             if (select == 0) {
                 System.out.println("=== Create a new Item ====");
-                System.out.print("Enter name: ");
                 String name = input.askStr("Enter name: ");
                 Item item = new Item(name);
                 tracker.add(item);
                 System.out.println("Added: " + item.getName() + " " + item.getId());
             } else if (select == 1) {
                 System.out.println("=== Show all items ====");
-                if (tracker.findAll().length != 0) {
-                    for (int index = 0; index < tracker.findAll().length; index++) {
-                        System.out.println("Find value: " + tracker.findAll()[index].getName() + " " + tracker.findAll()[index].getId());
+                Item[] findAll = tracker.findAll();
+                if (findAll.length != 0) {
+                    for (int index = 0; index < findAll.length; index++) {
+                        System.out.println("Find value: " + findAll[index].getName() + " " + findAll[index].getId());
                     }
                 } else {
                     System.out.println("Value not found");
                 }
             } else if (select == 2) {
                 System.out.println("=== Edit item ====");
-                System.out.print("Enter id: ");
                 String id = input.askStr("Enter id: ");
-                System.out.print("Enter name: ");
                 Item newItem = new Item(input.askStr("Enter name: "));
                 if (tracker.replace(id, newItem)) {
                     System.out.println("performed");
@@ -40,7 +40,6 @@ public class StartUI {
                 }
             } else if (select == 3) {
                 System.out.println("=== Delete item ====");
-                System.out.print("Enter id: ");
                 String id = input.askStr("Enter id: ");
                 if (tracker.delete(id)) {
                     System.out.println("performed");
@@ -49,20 +48,20 @@ public class StartUI {
                 }
             } else if (select == 4) {
                 System.out.println("=== Find item by Id ====");
-                System.out.print("Enter id: ");
                 String id = input.askStr("Enter id: ");
-                if (tracker.findById(id) != null) {
-                    System.out.println("Find value: " + tracker.findById(id).getName() + " " + tracker.findById(id).getId());
+                Item findById =  tracker.findById(id);
+                if (findById != null) {
+                    System.out.println("Find value: " + findById.getName() + " " + findById.getId());
                 } else {
                     System.out.println("Value not found");
                 }
             } else if (select == 5) {
                 System.out.println("=== Find items by name ====");
-                System.out.print("Enter name: ");
                 String name = input.askStr("Enter name: ");
-                if (tracker.findByName(name).length != 0) {
-                    for (int index = 0; index < tracker.findByName(name).length; index++) {
-                        System.out.println("Find value: " + tracker.findByName(name)[index].getName() + " " + tracker.findByName(name)[index].getId());
+                Item[] findByName = tracker.findByName(name);
+                if (findByName.length != 0) {
+                    for (int index = 0; index < findByName.length; index++) {
+                        System.out.println("Find value: " + findByName[index].getName() + " " + findByName[index].getId());
                     }
                 } else {
                     System.out.println("Value not found");
