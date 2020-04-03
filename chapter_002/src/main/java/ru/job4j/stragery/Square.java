4d14279da19fd0ba36d9cbaf27d042b843e9248e
VadimShein <shein.v94@mail.ru>
2020-04-03 13:39:51 +0600

    7. Шаблон проектирования - Стратегия.[#241229]

---
+++ chapter_002/src/main/java/ru/job4j/stragery/Square.java
@@ -0,0 +1,15 @@
+package ru.job4j.stragery;
+
+import java.util.StringJoiner;
+
+public class Square implements Shape {
+    @Override
+    public String draw() {
+        StringJoiner pic = new StringJoiner(System.lineSeparator());
+        pic.add("++++");
+        pic.add("+     +");
+        pic.add("+     +");
+        pic.add("++++" + "\r\n");
+        return pic.toString();
+    }
+}
