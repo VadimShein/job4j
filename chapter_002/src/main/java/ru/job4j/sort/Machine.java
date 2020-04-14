bd9e6c6cd922ce86e89c1f8f0c25d75d48d4a0cd
VadimShein <shein.v94@mail.ru>
2020-04-15 01:45:25 +0600

    Сдача в кофемашине [#34741]

---
+++ chapter_002/src/main/java/ru/job4j/sort/Machine.java
@@ -0,0 +1,26 @@
+package ru.job4j.sort;
+
+import java.util.Arrays;
+
+public class Machine {
+    private final int[] coins = {10, 5, 2, 1};
+
+    public int[] change(int money, int price) {
+        int[] rsl = new int[100];
+        int size = 0;
+        int numb = 0;
+        money -= price;
+        while (money > 0) {
+                if (money >= coins[numb]) {
+                    money -= coins[numb];
+                    rsl[size++] = coins[numb];
+                } else {
+                    numb++;
+                }
+        }
+        return Arrays.copyOf(rsl, size);
+    }
+}
+
+
+
