65d248e811a19f3167ea19f13187c192159cc518
VadimShein <shein.v94@mail.ru>
2020-04-17 18:46:50 +0600

    1. Телефонный справочник на базе ArrayList[#241182]

--- chapter_003/src/main/java/ru/job4j/search/PhoneDictionary.java
+++ chapter_003/src/main/java/ru/job4j/search/PhoneDictionary.java
@@ -20,8 +20,7 @@ public class PhoneDictionary {
         for (Person person : persons) {
            if (person.getName().contains(key) || person.getSurname().contains(key)
                     || person.getPhone().contains(key) || person.getAddress().contains(key)) {
-                result.add(new Person(person.getName(), person.getSurname(),
-                        person.getPhone(), person.getAddress()));
+                result.add(person);
             }
         }
         return result;
