f5d1d48862f77b06029abfb9126707150c08ba99
VadimShein <shein.v94@mail.ru>
2020-05-14 00:25:56 +0600

    Отсортировать департаменты[#241173]

--- chapter_003/src/main/java/ru/job4j/collection/DepDescComp.java
+++ chapter_003/src/main/java/ru/job4j/collection/DepDescComp.java
@@ -8,14 +8,7 @@ public class DepDescComp implements Comparator<String> {
         int rsl = 0;
         String[] o1Spl = o1.split("/");
         String[] o2Spl = o2.split("/");
-        int size = Math.min(o1Spl.length, o2Spl.length);
-
-        for (int index = 0; index < size; index++) {
-            rsl = o2Spl[0].compareTo(o1Spl[0]);
-            if (rsl != 0) {
-                break;
-            }
-        }
+        rsl = o2Spl[0].compareTo(o1Spl[0]);
         return rsl == 0 ? o1.compareTo(o2) : rsl;
     }
 }
