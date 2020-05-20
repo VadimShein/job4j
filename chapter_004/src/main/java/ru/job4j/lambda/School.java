12247c462625c45f99d1a1e6476289e1f8ca34c4
VadimShein <shein.v94@mail.ru>
2020-05-20 20:40:05 +0600

    1. Фильтрация учеников.[110058#241042]

--- chapter_004/src/main/java/ru/job4j/lambda/School.java
+++ chapter_004/src/main/java/ru/job4j/lambda/School.java
@@ -7,6 +7,6 @@ import java.util.stream.Collectors;
 public class School {
 
     public List<Student> collect(List<Student> students, Predicate<Student> predict) {
-        return students.stream().filter(predictX -> predict.test(predictX)).collect(Collectors.toList());
+        return students.stream().filter(predict).collect(Collectors.toList());
     }
 }
