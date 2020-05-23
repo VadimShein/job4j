1c0550bf38e1607b47972c34937a223dfc5a5cdd
VadimShein <shein.v94@mail.ru>
2020-05-23 22:04:01 +0600

    5. Преобразование матрицы чисел в список чисел. [#241046]

--- chapter_004/src/main/java/ru/job4j/lambda/MatrixTransform.java
+++ chapter_004/src/main/java/ru/job4j/lambda/MatrixTransform.java
@@ -1,11 +1,11 @@
 package ru.job4j.lambda;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class MatrixTransform {
-    public  List<Integer> matrixTr(List<List<Integer>> lists) {
-     //   return lists.stream().flatMap(e -> e.stream()).collect(Collectors.toList());
-        return lists.stream().flatMap(List::stream).collect(Collectors.toList());
+    public  List<Integer> matrixTr(Integer[][] matrix) {
+        return Arrays.stream(matrix).flatMap(Arrays::stream).collect(Collectors.toList());
     }
 }
--- chapter_004/src/test/java/ru/job4j/lambda/MatrixTransformTest.java
+++ chapter_004/src/test/java/ru/job4j/lambda/MatrixTransformTest.java
@@ -7,14 +7,14 @@ import java.util.List;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
-public class MattrixTransformTest {
+public class MatrixTransformTest {
 
     @Test
     public void matrixToList() {
-        List<List<Integer>> matrix = List.of(
-                List.of(1, 2),
-                List.of(3, 4)
-        );
+        Integer[][] matrix = {
+            {1, 2},
+            {3, 4}
+        };
         MatrixTransform res = new MatrixTransform();
         List<Integer> result = res.matrixTr(matrix);
         List<Integer> expected = List.of(1, 2, 3, 4);
