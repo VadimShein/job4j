bb9f3c0f8beb83c081e25e19fa26d75e3a083d21
VadimShein <shein.v94@mail.ru>
2020-05-29 00:31:43 +0600

    1. Collection API Улучшения [#241049]

--- chapter_003/src/test/java/ru/job4j/collection/ConvertListTest.java
+++ chapter_003/src/test/java/ru/job4j/collection/ConvertListTest.java
@@ -15,7 +15,7 @@ public class ConvertListTest {
         List<int[]> in = new ArrayList<>();
         in.add(new int[] {1});
         in.add(new int[] {2, 3});
-        List<Integer> expect = Arrays.asList(1, 2, 3);
+        List<Integer> expect = List.of(1, 2, 3);
         assertThat(ConvertList.convert(in), is(expect));
     }
 
@@ -24,7 +24,7 @@ public class ConvertListTest {
         List<int[]> in = new ArrayList<>();
         in.add(new int[] {});
         in.add(new int[] {2, 3, 4});
-        List<Integer> expect = Arrays.asList(2, 3, 4);
+        List<Integer> expect = List.of(2, 3, 4);
         assertThat(ConvertList.convert(in), is(expect));
     }
 }
--- chapter_003/src/test/java/ru/job4j/collection/DepartmentsTest.java
+++ chapter_003/src/test/java/ru/job4j/collection/DepartmentsTest.java
@@ -11,8 +11,8 @@ import static ru.job4j.collection.Departments.sortDesc;
 public class DepartmentsTest {
     @Test
     public void whenMissed() {
-        List<String> input = Arrays.asList("k1/sk1");
-        List<String> expect = Arrays.asList("k1", "k1/sk1");
+        List<String> input = List.of("k1/sk1");
+        List<String> expect = List.of("k1", "k1/sk1");
         List<String> result = Departments.fillGaps(input);
         sortAsc(result);
         assertThat(result, is(expect));
@@ -20,8 +20,8 @@ public class DepartmentsTest {
 
     @Test
     public void whenMissed2() {
-        List<String> input = Arrays.asList("k1/sk1/ssk1");
-        List<String> expect = Arrays.asList("k1", "k1/sk1", "k1/sk1/ssk1");
+        List<String> input = List.of("k1/sk1/ssk1");
+        List<String> expect = List.of("k1", "k1/sk1", "k1/sk1/ssk1");
         List<String> result = Departments.fillGaps(input);
         sortAsc(result);
         assertThat(result, is(expect));
@@ -29,8 +29,8 @@ public class DepartmentsTest {
 
     @Test
     public void whenMissed3() {
-        List<String> input = Arrays.asList("k1/sk1", "k2/sk2");
-        List<String> expect = Arrays.asList("k2", "k2/sk2", "k1", "k1/sk1");
+        List<String> input = List.of("k1/sk1", "k2/sk2");
+        List<String> expect = List.of("k2", "k2/sk2", "k1", "k1/sk1");
         List<String> result = Departments.fillGaps(input);
         sortDesc(result);
         assertThat(result, is(expect));
@@ -38,8 +38,8 @@ public class DepartmentsTest {
 
     @Test
     public void whenNonChange() {
-        List<String> input = Arrays.asList("k1", "k1/sk1");
-        List<String> expect = Arrays.asList("k1", "k1/sk1");
+        List<String> input = List.of("k1", "k1/sk1");
+        List<String> expect = List.of("k1", "k1/sk1");
         List<String> result = Departments.fillGaps(input);
         sortDesc(result);
         assertThat(result, is(expect));
